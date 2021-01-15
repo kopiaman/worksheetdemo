908f4e2a997ed0582f13034ec98edd7c1f573bd8
Salman <kopiaman@yahoo.com>
2021-01-15 18:50:54 +0800

    layout

--- src/views/Matching.vue
+++ src/views/Matching.vue
@@ -1,41 +1,57 @@
 <template>
   <div class="container mx-auto py-10">
-    <div
-      class="border m-6 rounded-lg bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden"
-    >
-      <div class="sm:flex sm:items-center px-6 py-4">
-        <img
-          class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
-          src="https://api.adorable.io/avatars/196/abott@adorable.png"
-          alt=""
-        />
-        <div class="text-center sm:text-left sm:flex-grow">
-          <div class="mb-4">
-            <p class="text-xl leading-tight">Jane Doe</p>
-            <p class="text-sm leading-tight text-grey-dark">
-              Software Developer at SpongeBob LLC.
-            </p>
-          </div>
-          <div class="flex flex-wrap">
-            <button
-              class="text-xs font-semibold rounded-full px-4 py-1 mx-3 leading-normal bg-white border border-blue text-blue hover:bg-blue hover:text-white"
-            >
-              Call
-            </button>
-            <button
-              class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white"
-            >
-              Message
-            </button>
-          </div>
-        </div>
+    <div class="flex" v-for="data in datas" :key="data.id">
+      <div
+        class="border m-2 rounded-lg bg-white mx-auto max-w-sm rounded-lg overflow-hidden p-3 flex items-center justify-center w-1/3"
+      >
+        <p class="text-xl leading-tight primary-color">{{ data.word }}</p>
+      </div>
+
+      <div></div>
+      <div
+        class="border m-2 rounded-lg bg-white mx-auto max-w-sm rounded-lg overflow-hidden p-3 w-1/3"
+      >
+        <p class="leading-tight secondary-color">
+          {{ data.description }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      datas: [
+        {
+          id: 1,
+          word: "CAR",
+          description:
+            "a road vehicle with an engine, four wheels, and seats for a small number of people",
+        },
+        {
+          id: 2,
+          word: "FLOWER",
+          description:
+            "the part of a plant that is often brightly coloured and has a pleasant smell,",
+        },
+        {
+          id: 3,
+          word: "PENCIL",
+          description:
+            "a long, thin object, usually made of wood, for writing or drawing, with a sharp black or coloured point at one end,",
+        },
+        {
+          id: 4,
+          word: "WALLET",
+          description:
+            "a small folding case for carrying paper money, credit cards and other flat objects, that can be carried in a pocket and is used especially by men:",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
